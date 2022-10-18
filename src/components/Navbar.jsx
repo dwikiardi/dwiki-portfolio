87a8837b5a1bd126d72a65d8287532ded2710f57
dwikiardi <dwiardi60@gmail.com>
2022-10-18 16:03:08 +0800

    Sidbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { BsFillMoonFill } from 'react-icons/bs';
+import { CgMenuRight } from 'react-icons/cg'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-between max-w-[1240px] mx-auto px-4 pt-2 text-white items-center'>
+    <div className='flex justify-between max-w-[1240px] mx-auto px-4 h-24 text-white items-center'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>Dwiki Portfolio.</h1>
       <ul className='flex items-start'>
         <li className='p-4'>Home</li>
@@ -12,6 +12,19 @@ const Navbar = () => {
         <li className='p-4'>About</li>
         <li className='p-4'>Contact</li>
       </ul>
+      <div>
+        <CgMenuRight size={20} />
+      </div>
+      <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Dwiki Portfolio.</h1>
+        <ul className='uppercase p-4'>
+          <li className='p-4 pt-0'>Home</li>
+          <li className='p-4'>Company</li>
+          <li className='p-4'>Resource</li>
+          <li className='p-4'>About</li>
+          <li className='p-4'>Contact</li>
+        </ul>
+      </div>
     </div>
   )
 }
